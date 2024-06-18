6b55023df5685b7af8450eeee7aed6ab64e1fa39
Akshi1910 <akshitha2004av@gmail.com>
2024-06-18 11:35:58 +0530

    Commited

---
+++ backend/routes/tours.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import { createTour,deleteTour,getSingleTour,updateTour,getAllTour,getTourBySearch,getFeaturedTour,getTourCount}from '../Controllers/tourControllers.js'
+import { verifyAdmin } from '../utils/verifyToken.js'
+const router = express.Router()
+
+//Create new tour 
+router.post('/', createTour)
+
+router.put("/:id",updateTour)
+router.delete("/:id",deleteTour)
+router.get("/:id",getSingleTour)
+router.get('/', getAllTour)
+router.get("/search/getTourBySearch", getTourBySearch)
+router.get("/search/getFeaturedTour", getFeaturedTour)
+router.get("/search/getTourCount", getTourCount)
+export default router
